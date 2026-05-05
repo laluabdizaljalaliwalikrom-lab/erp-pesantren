c90a5b08bc8b6ec24a9d5d568e78670eee281e07
laluabdizaljalaliwalikrom <laluabdizaljalaliwalikrom@gmail.com>
2026-05-05 11:19:12 +0800

    feat: add soft deletes to student_academic table migration

--- database/migrations/2026_03_24_022409_add_soft_deletes_to_student_academic.php
+++ database/migrations/2026_03_24_022409_add_soft_deletes_to_student_academic.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('student_academic', function (Blueprint $table) {
-            $table->softDeletes();
-        });
+        if (!Schema::hasColumn('student_academic', 'deleted_at')) {
+            Schema::table('student_academic', function (Blueprint $table) {
+                $table->softDeletes();
+            });
+        }
     }
 
     /**
